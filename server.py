7ab311f49019bdb7c056260b9152ce7ccf2783cf
SansTheMedic <59261388+SansTheMedic@users.noreply.github.com>
2024-05-16 13:27:53 +0100

    Sessions now store state
    all we need to do is get the live update

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import cherrypy
 import random
+import os, os.path
 
 class Session():
     # Initialization. Generates its storage and a random session code
@@ -10,9 +11,6 @@ class Session():
 
         # Generates a random 32-bit integer for the room code, excluding 0
         self.sessionCode = random.randint(1,2147483647)
-
-        # TEMP: test state body
-        self.state = "This is session: " + str(self.sessionCode)
     
     # Getter function for the session code
     def GetCode(self):
@@ -51,7 +49,7 @@ class Session():
             return "WARN: this user wasn't in this session"
 
 
-class HelloWorld(object):
+class Liveshare(object):
 
     def __init__(self):
         # Our dictionary of sessions. Each session will be keyed by its code and contain the session object
@@ -82,9 +80,65 @@ class HelloWorld(object):
         # Now we're sure there is a cookie for this user, we check if that user_id is in the session yet
         if this_user not in self.Sessions[session_code].GetUsers():
             self.Sessions[session_code].AddUser(this_user)
-        return self.Sessions[session_code].GetState()
+        
+        # Lastly we need to create the redirect to the session page
+        f = open("Session.html", "r")
+        session_page = f.read()
+        f.close()
 
-    # If a user is trying to join a session that doesn't exist, we call this to create a new one
+        # Add the session code
+        session_page = session_page.replace("[[SESSIONCODE]]", str(session_code))
+        # Add the current state
+        session_page = session_page.replace("[[CURRENTSTATE]]", self.Sessions[session_code].GetState())
+
+        return session_page
+    
+    # AJAX based call to handle any time the user makes an edit
+    @cherrypy.expose(["update"])
+    @cherrypy.tools.allow(methods=["POST"])
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def Update(self):
+        # Get our json input
+        input_json = cherrypy.request.json
+
+        # Extract our data
+        session_code = input_json["sessionCode"]
+        adds = input_json["adds"]
+        dels = input_json["dels"]
+
+        # Get the user from cookies
+        this_user = cherrypy.request.cookie["User_id"]
+
+        # Make sure that the user making this call is in the session
+        if self.Sessions[session_code].IsUser(this_user) == True:
+            # get the state of that session
+            current_state = self.Sessions[session_code].GetState()
+
+            # Handle deletions first
+            for each_del in dels:
+                index = int(each_del[1])
+                current_state = current_state[:index] + current_state[(index+1):]
+            # Then handle additions
+            for each_add in adds:
+                index = int(each_add[1])
+                char = each_add[0]
+                current_state = current_state[:index] + char + current_state[index:]
+            
+            # Update the stored session with this state
+            self.Sessions[session_code].SetState(current_state)
+
+            # Return our state to the user
+            return {"error": False,
+                    "msg": None,
+                    "newState": current_state}
+        else:
+            return {"error": True,
+                    "msg": "You aren't part of that session",
+                    "newState": None}
+
+
+    # If a user input_json[sessionCode]is trying to join a session that doesn't exist, we call this to create a new one
     def CreateSession(self):
         session_code = 0
         session_made = False
@@ -102,4 +156,15 @@ class HelloWorld(object):
         
         return session_code
 
-cherrypy.quickstart(HelloWorld())
+if __name__ == '__main__':
+    conf = {
+        '/': {
+            'tools.sessions.on': True,
+            'tools.staticdir.root': os.path.abspath(os.getcwd())
+        },
+        '/static': {
+            'tools.staticdir.on': True,
+            'tools.staticdir.dir': './public'
+        }
+    }
+    cherrypy.quickstart(Liveshare(), '/', conf)
